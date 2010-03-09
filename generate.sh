ad0877c1c352834301fb748f2b13ec15c97fc1e3
Liam Cooke <liamcooke@gmail.com>
2010-03-09 22:23:27 +0000

    Better rsync options

--- generate.sh
+++ generate.sh
@@ -6,10 +6,16 @@ LOGS="$EXTERNAL/data/log/irssi/freenode/#tumblrs/"
 CACHE="$EXTERNAL/cache/pisg"
 PISG="$HOME/local/src/pisg/0.72/pisg"
 OPTS="-co $PWD/tumblrs.cfg -ch '#tumblrs' -f irssi --cfg CacheDir=$CACHE --dir $LOGS"
+RSYNCOPTS="-az"
 y=$(date +%Y); m=$(date +%m); d=$(date +%d)
 
 [ -z $EXTERNAL/data ] && exit
-[ "$1" = "--cron" ] && OPTS="$OPTS -s"
+if [ "$1" = "--cron" ]; then
+    OPTS="$OPTS -s"
+    RSYNCOPTS="$RSYNCOPTS -q"
+else
+    RSYNCOPTS="$RSYNCOPTS -v --progress"
+fi
 
 rm -rf output
 mkdir -p $CACHE output/$y/$m
@@ -25,5 +31,5 @@ cp output/index.html $f
 chmod 644 output/*.html $f
 
 if [ "$1" = "-u" ] || [ "$1" = "--cron" ]; then
-    rsync -Pavz output/ boxofjunk:domains/tumblr.boxofjunk.ws/html/irc/
+    rsync $RSYNCOPTS output/ boxofjunk:domains/tumblr.boxofjunk.ws/html/irc/
 fi
